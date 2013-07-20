4956dff44330621c44c7827367a54c82f24dfdb6
Hassan Alin <superalin40@gmail.com>
2013-07-20 18:35:50 +0300

    Create Thread.lua

---
+++ Types/Thread.lua
@@ -0,0 +1,12 @@
+module(...,package.seeall)
+
+local function argCheck(var, t, argNum, funcName)
+   assert(type(var) == t, "bad argument #"..argNum.." to "..funcName.." ("..t.." expected, got "..type(var)..")");
+end
+
+debug.setmetatable(coroutine.create(function() end),{
+
+--Preventing the change of Lua Types
+__metatable = error("Protected Lua Types")
+
+})
